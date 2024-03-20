a6a92783190236b14d5524fb63477f7eeab225ce
Josh-Sharples <sharplesjd15@gmail.com>
2024-03-20 18:18:42 +0000

    this commit adds linkedin-github links

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import Photo from "./Photo";
 export default function() {
 
   return (
-    <div id="home" className="mt-60 ml-20 mr-20 home">
+    <div id="home" className="ml-20 mr-20 home">
       <Info/>
       <Photo/>
     </div>
